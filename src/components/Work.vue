fb8f7a8c990a7f7e27b530d037c60fb5a081b758
Kevin Davis <kldavis.244@gmail.com>
2019-10-16 16:09:54 -0500

    adjusting hover containers in Work

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -13,8 +13,8 @@
         </v-container>
         <v-container class="fill-height" fluid>
             <v-col cols="6">
-                <v-hover v-slot:default="{ hover }">
-                    <v-card>
+                <v-card>
+                    <v-hover v-slot:default="{ hover }">
                         <v-img
                             :src="id_image"
                             height="250"
@@ -35,24 +35,24 @@
                                 </div>
                             </v-expand-transition>
                         </v-img>
-                        <v-expansion-panels>
-                            <v-expansion-panel>
-                                <v-expansion-panel-header class="title">interior define</v-expansion-panel-header>
-                                <v-expansion-panel-content>
-                                    a magento-hosted, e-commerce store that is truly pleasing from a visual standpoint.
-                                    custom modules make browsing the product catalog a breeze, while the added swatch
-                                    store section allows customers to effortlessly pick which fabric is their favorite before purchase.
-                                    order data is captured by the connected laravel application, and an equally pleasing admin
-                                    panel allows employees to access said data with ease.
-                                </v-expansion-panel-content>
-                            </v-expansion-panel>
-                        </v-expansion-panels>
-                    </v-card>
-                </v-hover>
+                    </v-hover>
+                    <v-expansion-panels>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header class="title">interior define</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                                a magento-hosted, e-commerce store that is truly pleasing from a visual standpoint.
+                                custom modules make browsing the product catalog a breeze, while the added swatch
+                                store section allows customers to effortlessly pick which fabric is their favorite before purchase.
+                                order data is captured by the connected laravel application, and an equally pleasing admin
+                                panel allows employees to access said data with ease.
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </v-card>
             </v-col>
             <v-col cols="6">
-                <v-hover v-slot:default="{ hover }">
-                    <v-card>
+                <v-card>
+                    <v-hover v-slot:default="{ hover }">
                         <v-img
                             :src="gs_image"
                             height="250"
@@ -73,19 +73,19 @@
                                 </div>
                             </v-expand-transition>
                         </v-img>
-                        <v-expansion-panels>
-                            <v-expansion-panel>
-                                <v-expansion-panel-header class="title">grunt style</v-expansion-panel-header>
-                                <v-expansion-panel-content>
-                                    using html, liquid, and common js, this shopify theme is truly a custom experience.
-                                    reduced-cost for first-time subscription products are handled via ajax to push the
-                                    otherwise hidden products into the cart, and loads of examples of interaction with
-                                    the laravel application are scattered throughout the site.
-                                </v-expansion-panel-content>
-                            </v-expansion-panel>
-                        </v-expansion-panels>
-                    </v-card>
-                </v-hover>
+                    </v-hover>
+                    <v-expansion-panels>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header class="title">grunt style</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                                using html, liquid, and common js, this shopify theme is truly a custom experience.
+                                reduced-cost for first-time subscription products are handled via ajax to push the
+                                otherwise hidden products into the cart, and loads of examples of interaction with
+                                the laravel application are scattered throughout the site.
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </v-card>
             </v-col>
         </v-container>
         <v-container class="fill-height" fluid>
@@ -126,8 +126,8 @@
                 </v-card>
             </v-col>
             <v-col cols="6">
-                <v-hover v-slot:default="{ hover }">
-                    <v-card>
+                <v-card>
+                    <v-hover v-slot:default="{ hover }">
                         <v-img
                             :src="ag_image"
                             height="250"
@@ -148,25 +148,25 @@
                                 </div>
                             </v-expand-transition>
                         </v-img>
-                        <v-expansion-panels>
-                            <v-expansion-panel>
-                                <v-expansion-panel-header class="title">agweb</v-expansion-panel-header>
-                                <v-expansion-panel-content>
-                                    housing the nation's #1 site for agricultural content, is farm journal's top drupal
-                                    application. cron jobs pull content via API calls from the express js middleware application,
-                                    while keeping the site lightning-fast to make sure the user experience is top-notch.
-                                </v-expansion-panel-content>
-                            </v-expansion-panel>
-                        </v-expansion-panels>
-                    </v-card>
-                </v-hover>
+                    </v-hover>
+                    <v-expansion-panels>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header class="title">agweb</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                                housing the nation's #1 site for agricultural content, is farm journal's top drupal
+                                application. cron jobs pull content via API calls from the express js middleware application,
+                                while keeping the site lightning-fast to make sure the user experience is top-notch.
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </v-card>
             </v-col>
         </v-container>
         <v-container class="fill-height" fluid>
             <v-col cols="6">
-                <v-hover v-slot:default="{ hover }">
-                    <v-card>
-                        <v-img
+                <v-card>
+                    <v-hover v-slot:default="{ hover }">
+                            <v-img
                             :src="hh_image"
                             height="250"
                             contain
@@ -186,19 +186,19 @@
                                 </div>
                             </v-expand-transition>
                         </v-img>
-                        <v-expansion-panels>
-                            <v-expansion-panel>
-                                <v-expansion-panel-header class="title">harry helmet</v-expansion-panel-header>
-                                <v-expansion-panel-content>
-                                    written in the roots/bedrock wordpress boilerplate, this small site is full of custom
-                                    fields available from the admin panel. examples of said custom fields include:
-                                    testimonials (text and rating), footer copy, carousel images, hero headers/subtest,
-                                    and the hero promo bar.
-                                </v-expansion-panel-content>
-                            </v-expansion-panel>
-                        </v-expansion-panels>
-                    </v-card>
-                </v-hover>
+                    </v-hover>
+                    <v-expansion-panels>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header class="title">harry helmet</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                                written in the roots/bedrock wordpress boilerplate, this small site is full of custom
+                                fields available from the admin panel. examples of said custom fields include:
+                                testimonials (text and rating), footer copy, carousel images, hero headers/subtest,
+                                and the hero promo bar.
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </v-card>
             </v-col>
         </v-container>
     </div>
